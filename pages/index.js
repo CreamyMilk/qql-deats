4ba86ef3d563044a4f4be73a768ce804c2ef2e94
CreamyMilk <creamym254@gmail.com>
2022-10-10 19:26:22 +0300

    feat : added BId info

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+
     <div>
       <NavBar />
-      <div className="art-section">
-        <ArtPieceDetails />
-        <ArtPieceShowcase />
-      </div>
+      <ArtSection />
     </div>
   )
 }
 
+const ArtSection = () => {
+  return (
+    <div className="art-section">
+      <ArtPieceDetails />
+      <ArtPieceShowcase />
+    </div>
+  );
+}
 
 const NavBar = () => {
   return (
@@ -24,7 +30,6 @@ const NavBar = () => {
         className="search"
         placeholder="🔍 Collection, artist, item, address..."
       />
-
       <div>🍔 =</div>
     </div>
   );
@@ -32,16 +37,79 @@ const NavBar = () => {
 
 const ArtPieceShowcase = () => {
   return (
-  <div className="art-piece-showcase">
-      <Image src="https://static.archipelago.art/tokens/orig/artblocks/78/000887" alt="some cool dopamine trip" height={"1100"} width={"1000"} />
+    <div className="art-piece-showcase">
+      <Image src="/000887.png" alt="some cool dopamine trip" height={"1100"} width={"900"} />
     </div>)
+}
+
+const BidInfo = () => {
+  return(
+      <div className="bid-info">
+
+        <div className="price-info">
+          <small>
+            PRICE
+          </small>
+          <h1>Ξ 95</h1>
+          <p>
+            Expires in 4d
+          </p>
+          <button>
+            Purchase
+          </button>
+        </div>
+
+
+        <div className="highest-bid-info">
+          <small>
+            HIGH BID
+          </small>
+          <h1>Ξ 0.003</h1>
+          <p>
+            Expires in 4d
+          </p>
+          <button>
+            Place bid
+          </button>
+        </div>
+      </div>
+    );
+}
+
+
+const RoundButton = () => {
+  return(<>🌼</>);
 }
 
 const ArtPieceDetails = () => {
   return (
-  <div className="art-piece-details">
-    <ul>
-      <li>Hello world</li>
-    </ul>
-  </div>);
+    <div className="art-piece-details">
+      <h1 className="piece-id">#887</h1>
+      <a href="https://archipelago.art/collections/fidenza" className="collection-creator">Fidenza</a>
+      <h3 className="creator-name">Tyler Hobbs</h3>
+
+      <BidInfo/>
+
+      
+      <div className="section-title">
+        DESCRIPTION
+        <RoundButton />
+      </div>
+
+
+
+      FEATURES
+
+      RARITY
+
+      HISTORY
+
+      OWNER
+
+      0x74F...d97 →
+      Generator →
+
+
+
+    </div>);
 }
